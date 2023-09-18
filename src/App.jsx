7c826740d172f7514ee16e2b0084c2b3a7fca396
ZvEdward <zvonov.eddie@gmail.com>
2023-09-18 16:20:31 +0300

    router and placeholders for components done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+//hooks
+import { useState } from 'react'
+
+//components
+import LandingPage from LandingPage
+import SignUp from SignUp
+import HomePage from HomePage
+import Catalog from Catalog
+import ProductPage from ProductPage
+import ConfirmOrder from ConfirmOrder
+import OrderConfirmed from OrderConfirmed
+
+//libreries
+import { Router, Routes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Router>
+                <Routes path="/" element={ <LandingPage /> } />
+                <Routes path="/signUp" element={ <SignUp /> } />
+                <Routes path="/homePage" element={ <HomePage /> } />
+                <Routes path="/catalog" element={ <Catalog /> } />
+                <Routes path="/productPage" element={ <ProductPage /> } />
+                <Routes path="/confirmOrder" element={ <ConfirmOrder /> } />
+                <Routes path="/orderConfirmed" element={ <OrderConfirmed /> } />
+            </Router>
+        </>
+    )
 }
 
 export default App
