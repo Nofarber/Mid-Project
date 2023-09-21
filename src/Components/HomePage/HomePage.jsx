74c4ae3632454d9739a6f26709abf9322c34053c
omer nofarber <dr.fingerles@gmail.com>
2023-09-21 21:47:58 +0300

    fixed homepage login errors

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -4,39 +4,50 @@ import { useFurniture } from "../../furnitureContext"
 import ProductCard from '../ProductCard/ProductCard'
 import { useEffect } from "react"
 import './HomePage.css'
+import { useNavigate } from "react-router-dom"
 
 function HomePage() {
-const furnitureData = useFurniture()
-const furniture = furnitureData.furniture
-const credintials = useCredentials()
-const users = credintials.users
-const currentUser = credintials.currentUser
-const isConnected = credintials.isConnected
-const [orderedByNew,setOrderedByNew] = useState([])
-const infoOfConnected = users.find((value=>value.userName === currentUser.userName))
-const orderedFurniture = infoOfConnected.pickUpTime
-const [favoritesFurniture,setFavoritesFurniture] = useState([])
-
-useEffect(()=>{NewestFurniture(),WhoIsConnected()},[])
-
-function NewestFurniture(){
-setOrderedByNew(furniture.sort((a,b)=>a.publishDate < b.publishDate ? 1 : -1).slice(0,4))
-}
-
-function WhoIsConnected() {
-  const tempFurnArray = []
-  const fav = infoOfConnected.favorites
+  const nav = useNavigate()
+  const credintials = useCredentials()
+  const isConnected = credintials.isConnected
+  if (!isConnected) {
+    return(
+      <div>
+          <h1>.</h1>
+        <h1>back home</h1>
+        <button onClick={()=>nav('/')}>home</button>
+        </div>
+      )
+    } else {
+      const furnitureData = useFurniture()
+      const furniture = furnitureData.furniture
+      const users = credintials.users
+      const currentUser = credintials.currentUser
+      const [orderedByNew,setOrderedByNew] = useState([])
+      const infoOfConnected = users.find((value=>value.userName === currentUser.userName))
+      const orderedFurniture = infoOfConnected.pickUpTime
+      const [favoritesFurniture,setFavoritesFurniture] = useState([])
+      
+      useEffect(()=>{NewestFurniture(),WhoIsConnected()},[])
+      
+      function NewestFurniture(){
+        setOrderedByNew(furniture.sort((a,b)=>a.publishDate < b.publishDate ? 1 : -1).slice(0,4))
+      }
+      
+      function WhoIsConnected() {
+        const tempFurnArray = []
+        const fav = infoOfConnected.favorites
   fav.forEach((favTitle)=>{
     furniture.some((e)=>e.furnitureID===favTitle) && tempFurnArray.push(furniture.find((e)=>e.furnitureID === favTitle))
   })
-    setFavoritesFurniture(tempFurnArray)
-    console.log(tempFurnArray)
+  setFavoritesFurniture(tempFurnArray)
+  console.log(tempFurnArray)
 }
 
 
 
-  return (
-    <>
+return (
+  <>
     <div id="entire-page">
     <h1>!ברוך הבא</h1>
     <h2>:ראה מה חדש במחסן</h2>
@@ -53,5 +64,5 @@ function WhoIsConnected() {
     </div> 
     </>
   )
-}
+}}
 export default HomePage
